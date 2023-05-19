6f4d69eec195351f63003655c9ba1d0408f7b3a5
valinestelle <125728669+valinestelle@users.noreply.github.com>
2023-05-19 10:43:50 +0200

    ajout banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar.js';
 import './App.css';
+import Banner from './components/Banner.js';
 
 function App() {
   return (
+    <div>
     <Navbar />
+    <Banner />
+    </div>
+    
   )
 }
 
---
+++ src/components/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <img src='IMGbanner.png' alt='paysage'/>
+    </div>
+  )
+}
+
+export default Banner
