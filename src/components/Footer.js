44a68576dfe4302080f56b22ec91961c3fd0e253
valinestelle <125728669+valinestelle@users.noreply.github.com>
2023-05-19 18:41:06 +0200

    ajout footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <section className='footer'>
+       <div className='footer__logo'>
+          <img src="Logo2.png" alt="logo Kasa"/>
+       </div>
+       <div className='footer__text'>© 2020 Kasa. All right reserved</div>
+    </section>
+  )
+}
+
+export default Footer
