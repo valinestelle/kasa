62c55caf76cdcef93dd6eebfdc778f01f79045c8
valinestelle <125728669+valinestelle@users.noreply.github.com>
2023-05-19 13:28:23 +0200

    ajout Appartement grid

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from './components/Navbar.js';
 import './App.css';
 import Banner from './components/Banner.js';
+import Appartement from './components/Appartement.js';
 
 function App() {
   return (
     <div>
     <Navbar />
     <Banner />
+    <Appartement />
     </div>
     
   )
---
+++ src/components/Appartement.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Appartement.css'
+
+function Appartement() {
+  return (
+    <div className='grid'>hello</div>
+  )
+}
+
+export default Appartement
