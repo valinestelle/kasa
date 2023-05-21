aa00e8534e2875b77bb76ec8b6667cbed923e81f
valinestelle <125728669+valinestelle@users.noreply.github.com>
2023-05-21 17:15:13 +0200

    ajout fonction ds index.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import Navbar from './components/Navbar.js';
 import './App.css';
 import Banner from './components/Banner.js';
 import Appartement from './components/Appartement.js';
-import Footer from './components/Footer.js';
+
 
 function App() {
   return (
     <div>
     <main>
-    <Navbar />
     <Banner />
     <Appartement />
     </main>
-    <Footer />
     </div>   
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createBrowserRouter,  RouterProvider } from "react-router-dom";
+import { createBrowserRouter,  RouterProvider, Outlet } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
+const HeaderFooter = () => {
+  return <>
+  <main>
+     <Navbar />
+     <Outlet />
+  </main>
+     <Footer />
+  </>
+}
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
-    errorElement: <h1>404 not found</h1>
-  },
-  {
-    path: "/about",
-    element: <h1>A propos</h1>,
-  },
-    {
-    path: "/appartements",
-    element: <h1>Appartement</h1>,
-  },
-]); 
+    element: <HeaderFooter />,
+    errorElement: <h1>404 not found</h1>,
+    children: [
+      {
+        path: '/',
+        element: <App />,
+      },
+      {
+        path: '/about',
+        element: <h1>A propos</h1>,
+      },
+      {
+        path: '/appartements',
+        element: <h1>Appartements</h1>,
+      },
+    ]
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
